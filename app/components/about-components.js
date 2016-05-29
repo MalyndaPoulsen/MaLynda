a22bc61f8b01667699c1c80be955e04d2344a753
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-05-28 23:26:20 -0600

    target

--- app/components/about-components.js
+++ app/components/about-components.js
@@ -5,22 +5,35 @@
             templateUrl: 'app/components/about-components.html',
             controller: AboutController,
         })
-        
-        
-         function AboutController() {
+
+
+    function AboutController() {
         var ac = this;
-        var waypoint = new Waypoint({
-  element: document.getElementById('aboutme'),
-  handler: function(direction) {
+        
         ac.moveBar = function (id, num) {
-            $("#horse-"+id+"-progress").animate({ width: num+'px'}, 3000, 'linear');
-            }
-        ac.moveHorse = function (id, num) {
-            $("#horse-"+id).animate({ left: num+'px'}, 3000, 'linear'); 
-    console.log("finished running")
-            };
+            $("#horse-" + id + "-progress").animate({ 'width': num +'px'}, 3000, 'linear');
         }
-})
-  
+
+        ac.moveHorse = function (id, num) {
+            $("#horse-" + id).animate({ left: num +'px'}, 3000, 'linear');
+        };
+
+        // ac.moveBar = function (id, num) {
+        //     $("#horse-" + id + "-progress").animate({ width: num + 'px' }, 3000, 'linear');
+        // }
+
+        // ac.moveHorse = function (id, num) {
+        //     $("#horse-" + id).animate({ left: num + 'px' }, 3000, 'linear');
+        // };
+
+        var waypoint = new Waypoint({
+            element: document.getElementById('about-me'),
+            handler: function (direction) {
+                ac.moveBar(id, num);
+                ac.moveHorse(id, num );
+            }
+        })
+
+    }
 }
-}) ());
+    ());
---
+++ app/components/portfolio-components.js
@@ -0,0 +1,18 @@
+(function(){
+    angular.module('MaLynda')
+    .component('portfolioComponents', {
+        templateUrl: 'app/components/portfolio-components.html',
+        // controller: skillsController,
+    })
+    // function skillsController($firebaseArray){
+    //     var sc = this;
+    //     var skillsRef = new Firebase('https;favs15.firebaseio.com/ducks');
+    //     sc.addDuck = function (duck){
+    //         sc.row.$add(duck)
+    //         sc.newDuck = ''
+    //     }
+    //     sc.rotateDucks = function(duck){
+    //         sc.
+    //     }
+    // }
+}())
