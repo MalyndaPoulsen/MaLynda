2f8caf75ba46da6f0d52dee4bcbeb542bb5f5f5e
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-10 17:03:12 -0600

    CDN

---
+++ app/components/resume-components.js
@@ -0,0 +1,9 @@
+(function () {
+    angular.module('MaLynda')
+        .component('resumeComponents', {
+            templateUrl: 'app/components/resume-components.html',
+          
+        })
+
+
+}());
