8a2f080e70c79a2557c8f0ba0c6fb8cacbe4db12
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-11 14:54:45 -0600

    horses

--- app/components/about-components.js
+++ app/components/about-components.js
@@ -10,54 +10,87 @@
         var ac = this;
         var num = -8;
         var numb = 0;
+
+
         var paramsArray = [{
             id: 1,
             num: 81,
             numb: 93,
+            start: 0,
+            starter: 0,
         },
             {
                 id: 2,
                 num: 66,
                 numb: 77,
+                start: 0,
+                starter: 0,
             },
             {
                 id: 3,
                 num: 37,
                 numb: 46,
+                start: 0,
+                starter: 0,
             },
             {
                 id: 4,
-                num: 43,
-                numb: 37,
+                num: 50,
+                numb: 60,
+                start: 0,
+                starter: 0,
             },
             {
                 id: 5,
                 num: 70,
-                numb: 50,
+                numb: 81,
+                start: 0,
+                starter: 0,
             },
             {
                 id: 6,
                 num: 36,
-                numb: 40,
+                numb: 45,
+                start: 0,
+                starter: 0,
             },
             {
                 id: 7,
-                num: 51,
-                numb: 50,
+                num: 88,
+                numb: 100,
+                start: 0,
+                starter: 0,
             },
-         
-            ];
+
+        ];
 
 
 
         ac.moveBar = function (id, numb) {
-            $("#horse-" + id + "-progress").animate({ 'width': numb + '%' }, 3000, 'linear');
+            $("#horse-" + id + "-progress").animate({ 'width': numb + '%' }, 2700, 'linear');
         }
 
         ac.moveHorse = function (id, num) {
             $("#horse-" + id).animate({ left: num + '%' }, 3000, 'linear');
         };
 
+        ac.reset = function (id, start) {
+            $("#horse-" + id).animate({ left: start + '%' }, 1, 'linear');
+        };
+
+        ac.resetBar = function (id, starter) {
+            $("#horse-" + id + "-progress").animate({ 'width': starter + '%' }, 1, 'linear');
+        }
+
+        var reset = function () {
+            for (var i = 0; i < paramsArray.length; i++) {
+                var x = paramsArray[i].id;
+                var f = paramsArray[i].start;
+                var g = paramsArray[i].starter;
+                ac.reset(x, f);
+                ac.resetBar(x, g);
+            }
+        }
 
         var getParams = function () {
             for (var i = 0; i < paramsArray.length; i++) {
@@ -72,18 +105,16 @@
 
         }
 
-        var resetParams = function () {
-            var y = -40;
-            var d = 0;
-        }
+
+
         // reset horses
         var waypoint5 = new Waypoint({
             element: document.getElementById('big-deal'),
             handler: function (direction) {
                 if (direction === 'down') {
-                    resetParams()
-                    getParams();
-            }
+                    reset();
+                    // getParams();
+                }
             }
         })
 
@@ -92,8 +123,8 @@
             handler: function (direction) {
                 if (direction === 'down') {
                     getParams();
-                } else {
-                    resetParams()
+                } if(direction === 'up'){
+                    reset();
                 }
             }
         })
@@ -102,10 +133,9 @@
             element: document.getElementById('horse-4'),
             handler: function (direction) {
                 if (direction === 'up') {
-                    resetParams();
-                } else {
-               getParams();
-                }
+                    reset();
+                } 
+          
             }
         })
 
