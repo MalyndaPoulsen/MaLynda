39955e232a920504d0667f56c5b10fe3f5c3029a
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-26 08:57:09 -0600

    d

--- app/components/contact-components.js
+++ app/components/contact-components.js
@@ -2,7 +2,38 @@
     angular.module('MaLynda')
         .component('contactComponents', {
             templateUrl: 'app/components/contact-components.html',
+            controller: ContactController,
         })
 
+function ContactController(){
+var cc= this;
 
+   $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animationName).one(animationEnd, function () {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
+    document.getElementById("Print").onclick = function () {
+        printElement(document.getElementById("printThis"));
+    };
+
+    function printElement(elem) {
+        var domClone = elem.cloneNode(true);
+
+        var $printSection = document.getElementById("printSection");
+
+        if (!$printSection) {
+            var $printSection = document.createElement("div");
+            $printSection.id = "printSection";
+            document.body.appendChild($printSection);
+        }
+
+        $printSection.innerHTML = "";
+        $printSection.appendChild(domClone);
+        window.print();
+    }
+    }
 } ());
--- app/components/education-components.js
+++ app/components/education-components.js
@@ -7,33 +7,7 @@
 
          function EducationController() {
         var ec = this;
-    $.fn.extend({
-        animateCss: function (animationName) {
-            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            $(this).addClass('animated ' + animationName).one(animationEnd, function () {
-                $(this).removeClass('animated ' + animationName);
-            });
-        }
-    });
-    document.getElementById("Print").onclick = function () {
-        printElement(document.getElementById("printThis"));
-    };
-
-    function printElement(elem) {
-        var domClone = elem.cloneNode(true);
-
-        var $printSection = document.getElementById("printSection");
-
-        if (!$printSection) {
-            var $printSection = document.createElement("div");
-            $printSection.id = "printSection";
-            document.body.appendChild($printSection);
-        }
-
-        $printSection.innerHTML = "";
-        $printSection.appendChild(domClone);
-        window.print();
-    }
+ 
 
 
 
