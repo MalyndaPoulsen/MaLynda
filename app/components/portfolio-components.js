5b63e2b775fc67483b61ddc2de34ca8af32798fd
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-27 15:00:11 -0600

    fixes waypoint

--- app/components/portfolio-components.js
+++ app/components/portfolio-components.js
@@ -6,31 +6,6 @@
         })
     function PortfolioController() {
         var pc = this;
-
-
-       $.fn.extend({
-    animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-});
-        var waypoint6 = new Waypoint({
-            element: document.getElementById('boxes'),
-            handler: function (direction) {
-                if (direction === 'down') {
-                    $('#bookStore').animateCss('bounceInRight');
-                     $('#slap').animateCss('bounceInLeft');
-                      $('#todo').animateCss('bounceInDown');
-                       $('#CSS').animateCss('bounceInDown');
-                        $('#musicStore').animateCss('zoomIn');
-                         $('#Pop-Up').animateCss('slideInleft');
-                          $('#zooKeeper').animateCss('rollIn');
-                          $('#Pop-UpDevil').animateCss('zoomInUp');
-                }
-            }
-        })
     }
 }
     ())
--- app/components/skills-components.js
+++ app/components/skills-components.js
@@ -2,11 +2,38 @@
     angular.module('MaLynda')
         .component('skillsComponents', {
             templateUrl: 'app/components/skills-components.html',
-          
+            controller: SkillsController
         })
 
+    function SkillsController() {
+        this.$onInit = function(){
+            $.fn.extend({
+                animateCss: function (animationName) {
+                    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                    $(this).addClass('animated ' + animationName).one(animationEnd, function () {
+                        $(this).removeClass('animated ' + animationName);
+                    });
+                }
+            });
+            var waypoint6 = new Waypoint({
+                element: document.getElementById('boxes'),
+                handler: function (direction) {
+                    if (direction === 'down') {
+                        $('#bookStore').animateCss('bounceInRight');
+                        $('#slap').animateCss('bounceInLeft');
+                        $('#todo').animateCss('bounceInDown');
+                        $('#CSS').animateCss('bounceInDown');
+                        $('#musicStore').animateCss('zoomIn');
+                        $('#Pop-Up').animateCss('slideInleft');
+                        $('#zooKeeper').animateCss('rollIn');
+                        $('#Pop-UpDevil').animateCss('zoomInUp');
+                    }
+                }
+            })
+        }
+    }
 
-}());
+} ());
 
 
 
